abc6c3a24a0912e5c86f5e18de285e46d0bf19d1
Anderson Lima <anderson.sl93@hotmail.com>
2016-09-27 13:22:37 -0300

    doskeys

---
+++ doskeys.bat
@@ -0,0 +1,34 @@
+@echo off
+set "PATH=%PATH%;%USERPROFILE%\Documents\python;%USERPROFILE%\Documents\python\Scripts;%USERPROFILE%\Documents\python2;%USERPROFILE%\Documents\vim\vim80"
+
+REM system alias
+doskey clear=cls
+doskey ls=dir $*
+doskey :q=exit
+doskey :q!=exit
+doskey send=pscp -r $1 $2@$3:$4
+
+REM environment
+doskey cp=copy $*
+doskey p=python $*
+doskey wo=workon $*
+doskey pipt=pip install --trusted-host pypi.python.org  $*
+doskey pipit=pip install --index-url=http://pypi.python.org/simple/ --trusted-host pypi.python.org  $*
+
+REM django alias
+doskey pm=python manage.py $*
+doskey dr=python manage.py runserver 0.0.0.0:8000
+doskey pms=python manage.py shell
+doskey pmm=python manage.py migrate $*
+doskey pmmk=python manage.py makemigrations $*
+doskey pmcs=python manage.py createsuperuser
+
+REM git alias
+doskey g=git $*
+doskey gs=git status
+doskey ga=git add $*
+doskey gd=git diff $*
+doskey gb=git branch $*
+doskey gc=git commit -m $*
+doskey gps=git push origin $*
+doskey gpl=git pull origin $*
